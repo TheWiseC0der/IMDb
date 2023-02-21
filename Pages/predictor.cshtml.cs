5ad5d82f5d3172698ff161f764eca4f0de03b323
C0deSn@ke <bryan.koolman@student.nhlstenden.com>
2023-02-21 17:15:51 +0100

    made some pages for later

---
+++ Pages/predictor.cshtml.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace IMDb.Pages
+{
+    public class PredictorModel : PageModel
+    {
+        private readonly ILogger<QuerierModel> _logger;
+
+        public PredictorModel(ILogger<QuerierModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+
+        }
+    }
+}

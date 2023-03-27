faa4d60566a6a8d317b1a72256a6e2f6ac88876a
Patrick-Kuipers <patrick_k1994@hotmail.nl>
2023-03-27 17:30:50 +0200

    PersonPlaysGenre

--- Pages/Query.cshtml.cs
+++ Pages/Query.cshtml.cs
@@ -1,14 +1,17 @@
 ﻿using IMDb.Models;
 using IMDb.Repositories;
+using IMDb.Views;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.AspNetCore.Mvc.Rendering;
+using System.Collections.Generic;
 
 namespace IMDb.Pages
 {
     public class QueryModel : PageModel
     {
         private readonly CrudRepository _crudRepository;
+        public List<PersonGenreRating> personPlaysInGenres { get; set; }
         private readonly ILogger<QueryModel> _logger;
         public QueryModel(CrudRepository crudRepository, ILogger<QueryModel> logger)
         {
@@ -20,8 +23,9 @@ namespace IMDb.Pages
         
         public List<Genre>? Genres { get; set; }
 
-        public void OnGet()
+        public async Task OnGet()
         {
+            personPlaysInGenres = await _crudRepository.ReadAllRows<PersonGenreRating>();
         }
     }
 }
---
+++ Views/PersonPlaysInGenre.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace IMDb.Views
+{
+    [Table("PersonGenreRating")]
+    [Keyless]
+    public class PersonGenreRating
+    {
+        public string persoon { get; set; }
+        public string genre { get; set; }
+        public double gemiddelde_rating { get; set; }
+    }
+}
